4a4d34cd297cd2fee9890d4e7853b9843f85c8a1
ZhouWei <1244620067@qq.com>
2024-06-29 23:44:53 +0800

    feat: add request

---
+++ src/services/request.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import type {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestHeaders,
+  AxiosResponse,
+  InternalAxiosRequestConfig
+} from 'axios';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: 'https://api.github.com',
+  timeout: 1000,
+});
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
